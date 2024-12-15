ad20cb201f03fbf418e7b9d575055f36becebea2
sala910 <Matesalazar9@gmail.com>
2024-12-16 01:03:00 +0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array = { 4, 10, 3, 5, 1 };
+
+        Console.WriteLine("Исходный массив:");
+        PrintArray(array);
+
+        HeapSort(array);
+
+        Console.WriteLine("\nОтсортированный массив:");
+        PrintArray(array);
+    }
+
+    static void HeapSort(int[] array)
+    {
+        int n = array.Length;
+
+        for (int i = n / 2 - 1; i >= 0; i--)
+        {
+            Heapify(array, n, i);
+        }
+
+        for (int i = n - 1; i >= 0; i--)
+        {
+            Swap(array, 0, i);
+            Heapify(array, i, 0);
+        }
+    }
+
+    static void Heapify(int[] array, int n, int i)
+    {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (left < n && array[left] > array[largest])
+        {
+            largest = left;
+        }
+
+        if (right < n && array[right] > array[largest])
+        {
+            largest = right;
+        }
+
+        if (largest != i)
+        {
+            Swap(array, i, largest);
+            Heapify(array, n, largest);
+        }
+    }
+
+    static void Swap(int[] array, int i, int j)
+    {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    static void PrintArray(int[] array)
+    {
+        foreach (var item in array)
+        {
+            Console.Write(item + " ");
+        }
+        Console.WriteLine();
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("Алгоритм сортировки heapsort")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Алгоритм сортировки heapsort")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("240a4330-9e54-4c0e-829b-769dbf1af9f4")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
